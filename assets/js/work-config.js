33ef4e3f300c35a74fa7e6f98d381bc8dc6abaee
Munajt <60009613+czoPredato@users.noreply.github.com>
2026-06-13 18:29:58 +0200

    Update work-config.js

--- assets/js/work-config.js
+++ assets/js/work-config.js
@@ -19,7 +19,7 @@ var WORK_CATEGORIES = [
 		slug: 'thumbnails',
 		label: 'Thumbnails',
 		folder: 'thumbnails',
-		images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg']
+		images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg']
 	},
 	{
 		slug: 'business-cards',
